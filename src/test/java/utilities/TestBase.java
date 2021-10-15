0fa554de795e1dd791dbdfd5e2b930568c06bdec
Muhammed Solmazgul <mmsolmazgul039@gmail.com>
2021-10-15 22:45:41 +0200

    C05_FileUpload

--- src/test/java/utilities/TestBase.java
+++ src/test/java/utilities/TestBase.java
@@ -9,7 +9,7 @@ public abstract class TestBase {
     // abstract yaparak bu class'dan obje olusturulmasinin onune geceriz
     protected WebDriver driver;
     // biz test base class'i sadece extends ile inherit ederek kullanacagiz
-    // dolayisiyla olusturdugumuz driver variable'i icin protected access modifier'i seciyoruz..
+    // dolayisiyla olusturdugumuz driver variable'i icin protected access modifier'i seciyoruz...
     @BeforeClass
     public void setup(){
         WebDriverManager.chromedriver().setup();
