cb333c3aade7387ec9071879aac984ada5a5d9bf
Muhammed Solmazgul <mmsolmazgul039@gmail.com>
2021-10-15 20:47:15 +0200

    C04_WindowHandle

--- src/test/java/tests/day09/C04_WindowHandle.java
+++ src/test/java/tests/day09/C04_WindowHandle.java
@@ -10,7 +10,7 @@ public class C04_WindowHandle extends TestBase {
         // ● Tests package’inda yeni bir class olusturun: C04_WindowHandle
         //● https://the-internet.herokuapp.com/windows adresine gidin.
         driver.get("https://the-internet.herokuapp.com/windows");
-        //● Sayfadaki textin “Opening a new window” olduğunu doğrulayın.
+        //● Sayfadaki textin “Opening a new window” olduğunu doğrulayın...
         String actualYazi=driver.findElement(By.tagName("h3")).getText();
         String expectedYazi="Opening a new window";
         SoftAssert softAssert=new SoftAssert();
